d44f636ba0d6f601ce30ab06fe31f7a62f457307
yogeshwaran <yokeshwaran11102002@gmail.com>
2025-04-10 14:18:29 +0530

    selector quantity changes finished

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -62,9 +62,15 @@ document.querySelector('.js-products-grid').
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
+      
+      
       const productId = button.dataset.productId;
 
       let matchingItem;
+
+      const productSelector = document.querySelector(`.js-quantity-selector-${productId}`);
+
+      const quantity = Number(productSelector.value);
 
       cart.forEach((item) => {
         if(productId === item.productId){
@@ -73,11 +79,11 @@ document.querySelectorAll('.js-add-to-cart')
       });
 
       if(matchingItem){
-        matchingItem.quantity += 1;
+        matchingItem.quantity += quantity;
       }else {
         cart.push({
           productId : productId,
-          quantity: 1
+          quantity: quantity
         });
       }
 
