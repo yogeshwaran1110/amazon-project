e5a3f6fe3895948cb60ea8848ab2577413fdd993
yogeshwaran <yokeshwaran11102002@gmail.com>
2025-04-18 21:23:43 +0530

    14 class finished

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
-import { cart,addToCart} from '../data/cart.js';
+import { cart,addToCart,
+  calculateCartQuantity} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -65,16 +66,14 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 const addedMessageTimeouts = {};
 
 function updateCartQuantity(){
-  let cartQuantity = 0;
-
-    cart.forEach((cartItem) => {
-      cartQuantity += cartItem.quantity;
-    });
+  
+  const cartQuantity = calculateCartQuantity();
 
 
     document.querySelector('.js-cart-quantity')
-      .innerHTML = cartQuantity;
+      .innerHTML = cartQuantity || '';
 }
+
 
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
