8709a69a9725c086af5b68e57a0e7464266ee8a5
yogeshwaran <yokeshwaran11102002@gmail.com>
2025-04-28 16:07:18 +0530

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -5,12 +5,8 @@ import { formatCurrency } from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import {deliveryOptions, getDeliveryOption}from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
-hello();
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
 
 export function renderOrderSummary(){
 
@@ -130,6 +126,9 @@ export function renderOrderSummary(){
 
                 const container = document.querySelector(`.js-cart-item-container-${productId}`);
                 container.remove();
+
+                renderPaymentSummary();
+
                 updateCartQuantity ();
             });
         });
@@ -140,6 +139,7 @@ export function renderOrderSummary(){
                 const{productId,deliveryOptionId}= element.dataset;
                 updateDeliveryOption(productId,deliveryOptionId);
                 renderOrderSummary();
+                renderPaymentSummary();
             })
         })
 
