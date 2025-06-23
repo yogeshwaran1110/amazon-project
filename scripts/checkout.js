574c88d5020bf73e116c90ca4a0886ccd22b8b7e
yogeshwaran <yokeshwaran11102002@gmail.com>
2025-06-23 16:05:21 +0530

    add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,7 @@ import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import '../data/car.js';
 // import '../data/cart-class.js';
+import '../data/backend-practice.js';
 
 renderCheckoutHeader();
 renderOrderSummary();
